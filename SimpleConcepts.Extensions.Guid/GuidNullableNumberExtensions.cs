3c258661ead4b15f48ea0ddecb16d88b30a26c86
Raphael Abreu <raphael.lorenzeto@gmail.com>
2020-05-22 09:34:52 -0300

    Adding numeric nullable support.

---
+++ SimpleConcepts.Extensions.Guid/GuidNullableNumberExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleConcepts.Extensions
+{
+    public static class GuidNullableNumberExtensions
+    {
+        public static Guid? ToGuid(this long? input)
+        {
+            return input?.ToGuid();
+        }
+
+        public static Guid? ToGuid(this int? input)
+        {
+            return input?.ToGuid();
+        }
+
+        public static Guid? ToGuid(this short? input)
+        {
+            return input?.ToGuid();
+        }
+
+        public static long? ToInt64(this Guid? input)
+        {
+            return input?.ToInt64();
+        }
+
+        public static int? ToInt32(this Guid? input)
+        {
+            return input?.ToInt32();
+        }
+
+        public static short? ToInt16(this Guid? input)
+        {
+            return input?.ToInt16();
+        }
+    }
+}
